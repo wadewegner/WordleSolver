bca82d3f2a7a94cf4bd7e8b71d0c41ff5362fb09
Wade Wegner <wade.wegner@gmail.com>
2023-04-07 08:29:32 -0700

    updated

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
-namespace WordleSolver.Pages;
-
-public class IndexModel : PageModel
+namespace WordleSolver.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
+   public class Letter
+{
+    public char Character { get; set; }
+    public string Color { get; set; } = "green";
+}
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public class Word
     {
-        _logger = logger;
+        public List<Letter> Letters { get; set; } = new List<Letter>();
     }
 
-    public void OnGet()
+    public class IndexModel : PageModel
     {
+        [BindProperty]
+        public List<Word> Words { get; set; } = new List<Word>();
 
-    }
+        public string ResultMessage { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            // Clear the words list before adding new words
+            Words.Clear();
+
+            // Collect the data from the form into a list of six words
+            for (int i = 0; i < 6; i++)
+            {
+                Word word = new Word();
+                for (int j = 0; j < 5; j++)
+                {
+                    // Use Request.Form to get the value of each input by its name attribute
+                    string inputValue = Request.Form[$"word-{i}-letter-{j}"].ToString().Trim();
+                    char letterChar = !string.IsNullOrEmpty(inputValue) ? inputValue[0] : ' ';
+                    string letterColor = Request.Form[$"word-{i}-letter-{j}-color"].ToString().Trim();
+
+                    // Save the input values and colors to TempData
+                    TempData[$"word-{i}-letter-{j}"] = letterChar;
+                    TempData[$"word-{i}-letter-{j}-color"] = letterColor;
+
+                    word.Letters.Add(new Letter { Character = letterChar, Color = letterColor });
+                }
+                Words.Add(word);
+            }
+        }
+   }
 }
