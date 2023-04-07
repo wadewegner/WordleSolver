3fbe8328c255e20cbd269f20a7438c0b090d69dd
Wade Wegner <wade.wegner@gmail.com>
2023-04-06 18:45:28 -0700

    fixing

--- Pages/WordleSolver.cshtml.cs
+++ Pages/WordleSolver.cshtml.cs
@@ -4,10 +4,21 @@ using System.Collections.Generic;
 
 namespace WordleSolver.Pages
 {
+   public class Letter
+{
+    public char Character { get; set; }
+    public string Color { get; set; } = "green";
+}
+
+    public class Word
+    {
+        public List<Letter> Letters { get; set; } = new List<Letter>();
+    }
+
     public class WordleSolverModel : PageModel
     {
         [BindProperty]
-        public List<string> Words { get; set; } = new List<string>();
+        public List<Word> Words { get; set; } = new List<Word>();
 
         public string ResultMessage { get; set; }
 
@@ -23,12 +34,15 @@ namespace WordleSolver.Pages
             // Collect the data from the form into a list of six words
             for (int i = 0; i < 6; i++)
             {
-                string word = "";
+                Word word = new Word();
                 for (int j = 0; j < 5; j++)
                 {
                     // Use Request.Form to get the value of each input by its name attribute
-                    string letter = Request.Form[$"word-{i}-letter-{j}"];
-                    word += letter;
+                    char letterChar = Request.Form[$"word-{i}-letter-{j}"].ToString()[0];
+
+                    string letterColor = Request.Form[$"word-{i}-letter-{j}-color"];
+
+                    word.Letters.Add(new Letter { Character = letterChar, Color = letterColor });
                 }
                 Words.Add(word);
             }
