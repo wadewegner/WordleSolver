22a35a64e5ad3e960f34f748208b37372990c1e6
Wade Wegner <wade.wegner@gmail.com>
2023-04-09 10:39:13 -0700

    Clarified tests

--- Tests/SuggestionEngineTests.cs
+++ Tests/SuggestionEngineTests.cs
@@ -20,6 +20,7 @@ namespace WordleSolver.Tests
         [Fact]
         public void GetWordleWords_ShouldReturnCorrectResults()
         {
+            // check if the wordlist is not null and has the correct number of words
             wordList.Should().NotBeNull();
             wordList.Should().NotBeEmpty();
             wordList.Should().HaveCount(14855);
@@ -30,6 +31,7 @@ namespace WordleSolver.Tests
         {
             var suggestionEngine = new SuggestionEngine(_wordDictionaryService);
 
+            // construct the words with the correct colors
             var wordDetails = new[]
             {
                 ("dealt", "ddddg"),
@@ -37,12 +39,16 @@ namespace WordleSolver.Tests
                 ("torot", "dgddg")
             };
 
+            // convert the word details to a list of words
             var words = EngineHelper.ConstructWords(wordDetails);
+            // get the most likely words
             var result = suggestionEngine.GetTop10LikelyWords(words);
 
-            // Assert
+            // check if the result is not null nor empty
             result.Should().NotBeNull();
             result.Should().NotBeEmpty();
+
+            // check that the result does not contain the word "torot"
             result.Should().NotContain("TOROT");
         }
     }
